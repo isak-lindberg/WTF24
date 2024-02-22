5def36f0cf0414b2081af646f8c7ba3e294b27a1
isak-lindberg <119052320+isak-lindberg@users.noreply.github.com>
2024-02-22 16:08:18 +0100

    användarbegränsade funktioner

--- app.rb
+++ app.rb
@@ -1,55 +1,66 @@
 require 'debug'
 
 class App < Sinatra::Base
+    enable :sessions
+        def db
+            if @db == nil
+                @db = SQLite3::Database.new('./db/db.sqlite')
+                @db.results_as_hash = true
+            end
+            return @db
 
-    def db
-        if @db == nil
-            @db = SQLite3::Database.new('./db/db.sqlite')
-            @db.results_as_hash = true
+        before do
+            @current_user_id = 1
+            @current_user_access = db.execute('SELECT access FROM users WHERE id = ?', @current_user_id).first
         end
-        return @db
-    end
 
-    get '/' do
-        erb :'books/index'
-    end
+        get '/' do
+            erb :'books/index'
+        end
 
-    post '/books/new' do 
-        name = params['name'] 
-        description = params['description']
-        query = 'INSERT INTO books (name, description) VALUES (?, ?) RETURNING *'
-        result = db.execute(query, name, description).first 
-        redirect "/books/#{result['id']}" 
-    end
+        post '/books/new' do 
+            name = params['name'] 
+            description = params['description']
+            query = 'INSERT INTO books (name, description) VALUES (?, ?) RETURNING *'
+            result = db.execute(query, name, description).first 
+            redirect "/books/#{result['id']}" 
+        end
 
-    get '/books/:id/update' do |id|
-        @book = db.execute('SELECT * FROM books WHERE id = ?', id).first
-        #binding.break
-        erb :'books/update'
-    end
+        get '/books/:id/update' do |id|
+            @book = db.execute('SELECT * FROM books WHERE id = ?', id).first
+            #binding.break
+            erb :'books/update'
+        end
 
-    post '/books/:id/update' do |id| 
-        book_name = params['name']
-        book_description = params['description']
-        db.execute('UPDATE books SET name = ?, description = ? WHERE id = ?', book_name, book_description, id)
-        redirect "/books/#{id}" 
-    end
+        post '/books/:id/update' do |id| 
+            book_name = params['name']
+            book_description = params['description']
+            db.execute('UPDATE books SET name = ?, description = ? WHERE id = ?', book_name, book_description, id)
+            redirect "/books/#{id}" 
+        end
 
-    post '/books/:id/delete' do |id| 
-        db.execute('DELETE FROM books WHERE id = ?', id)
-        redirect "/books"
-      end
+        post '/books/:id/delete' do |id| 
+            db.execute('DELETE FROM books WHERE id = ?', id)
+            redirect "/books"
+        end
 
-    get '/books' do
-        @books = db.execute('SELECT * FROM books;')
-        erb :'books/books'
-    end
+        post '/books/:id/rate' do |id|
+            rating = params['rating'].to_i 
+            query = 'INSERT INTO ratings (rating) VALUES (?) RETURNING id'
+            result = db.execute(query, rating).first 
+            # Att fixa nästa gång:
+            # Varje unik användare måste kunna ha en egen rating av varje unik bok. Försök fixa detta. 
+        end
 
-    get '/books/:id' do |id|
-        @book = db.execute('SELECT * FROM books WHERE id = ?', id).first
-        @user = db.execute('SELECET access FROM users WHERE ') #FORTSÄTT HÄR!
-        erb :'books/show'
-    end
+        get '/books' do
+            @books = db.execute('SELECT * FROM books;')
+            erb :'books/books'
+        end
+
+        get '/books/:id' do |id|
+            @book = db.execute('SELECT * FROM books WHERE id = ?', id).first
+            erb :'books/show'
+        end
 
     
 end
